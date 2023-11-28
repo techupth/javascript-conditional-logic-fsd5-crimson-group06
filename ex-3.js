c1c55bf1cfdceba98196b16d3b06b95b45454347
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-28 15:36:17 +0700

    feat(ex-2, 3): add condition for 'off' in ex-2, completed ex-3

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,12 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+lightBulbStatus === "On"
+    ? console.log("Light bulb is On.")
+    : console.log("Please choose the correct input (On/Off)");
+lightBulbStatus === "Off"
+    ? console.log("Light bulb is Off.")
+    : console.log("Please choose the correct input (On/Off)");
+lightBulbStatus === "Broken"
+    ? console.log("Light bulb is Broken.")
+    : console.log("Please choose the correct input (On/Off)");
