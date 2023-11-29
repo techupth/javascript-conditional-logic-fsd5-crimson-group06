82171ca02c07fba0cf385df7196d4c42a441784f
Kaew <rajitkaew2285@gmail.com>
2023-11-29 16:41:51 +0700

    feat(ex4): add complete ex4

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,17 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+switch (lightBulbStatus) {
+  case "On":
+    lightBulbStatus = "Light bulb is On.";
+    break;
+  case "Off":
+    lightBulbStatus = "Light bulb is Off.";
+    break;
+  case "Broken":
+    lightBulbStatus = "Light bulb is Broken.";
+    break;
+  default:
+    lightBulbStatus = "Please choose the correct input (On/Off/Broken)";
+}
+console.log(lightBulbStatus);
